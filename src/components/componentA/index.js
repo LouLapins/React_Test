caf7283e85b72463af14cf7d03d5b04da7b6b040
LouLapins <loulapins@hotmail.com>
2021-04-14 18:26:52 +0200

    Task 3 color buttons

--- src/components/componentA/index.js
+++ src/components/componentA/index.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 
 export default class ComponentA extends Component {
+
+    state = {color: "green"};
+
+    changeColor = () => {
+        this.setState({ color: "pink" });
+    }
+
     render() {
         return (
-            <div className={"component"}>
+            <div className={"component"} id={this.state.color}>
                 <span className={"component_title"}> Component A </span>
+                <button className={"color_btn"} onClick={this.changeColor}>Change color</button>
             </div>
         )
     }
--- src/components/componentB/index.js
+++ src/components/componentB/index.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 
 export default class ComponentB extends Component {
+    state = {color: "green"};
+
+    changeColor = () => {
+        this.setState({ color: "pink" });
+    }
+
     render() {
         return (
-            <div className={"component"}>
-                <span className={"component_title"}> Component B </span>
+            <div className={"component"} id={this.state.color}>
+                <span className={"component_title"}> Component A </span>
+                <button className={"color_btn"} onClick={this.changeColor}>Change color</button>
             </div>
         )
     }

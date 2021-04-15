b42a008a0e0323bba6b53fae94ce8c6b911a366c
LouLapins <loulapins@hotmail.com>
2021-04-15 17:15:15 +0200

    Task 4 persist color change

--- src/components/componentA/index.js
+++ src/components/componentA/index.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react'
 
 export default class ComponentA extends Component {
-
-    state = {color: "green"};
-
-    changeColor = () => {
-        this.setState({ color: "pink" });
+    constructor(props) {
+        super(props);
+        this.state = {color: "green"}
+        this.changeColor = this.changeColor.bind(this);
     }
+
+   componentDidMount() {
+        const color = localStorage.getItem("colorA");
+        this.setState({ color: color });
+   }
+
+   componentDidUpdate() {
+        localStorage.setItem("colorA", this.state.color);
+   }
+
+   changeColor() {
+       this.setState((prevState) => {
+        if (prevState.color === "green") {
+            return { color: "pink" };
+        } else {
+            return { color: "green" };
+        }
+        });
+       }
+   
 
     render() {
         return (
--- src/components/componentB/index.js
+++ src/components/componentB/index.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react'
 
 export default class ComponentB extends Component {
-    state = {color: "green"};
+    constructor(props) {
+        super(props);
+        this.state = { color: "green"}
+        this.changeColor = this.changeColor.bind(this);
+    }
 
-    changeColor = () => {
-        this.setState({ color: "pink" });
+   componentDidMount() {
+        const color = localStorage.getItem("colorB");
+        this.setState({ color: color });
+   }
+
+   componentDidUpdate() {
+        localStorage.setItem("colorB", this.state.color);
+   }
+
+   changeColor() {
+    this.setState((prevState) => {
+     if (prevState.color === "green") {
+         return { color: "pink" };
+     } else {
+         return { color: "green" };
+     }
+     });
     }
 
     render() {
         return (
             <div className={"component"} id={this.state.color}>
-                <span className={"component_title"}> Component A </span>
+                <span className={"component_title"}> Component B </span>
                 <button className={"color_btn"} onClick={this.changeColor}>Change color</button>
             </div>
         )
